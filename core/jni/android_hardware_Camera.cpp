Colt-Oreo/android_frameworks_base-1

FB: Add oneplus camera support
Change-Id: I8f247dd48a0eca610d2781b03c0fa84713adaf46
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

core/jni/android_hardware_Camera.cpp
@@ -43,6 +43,7 @@ enum {
 
 struct fields_t {
     jfieldID    context;
+    jfieldID    metadata_ptr;
     jfieldID    facing;
     jfieldID    orientation;
     jfieldID    canDisableShutterSound;
@@ -66,6 +67,7 @@ struct fields_t {
 
 static fields_t fields;
 static Mutex sLock;
+static CameraMetadata* mMeta_ptr;
 
 // provides persistent context for calls from native code to Java
 class JNICameraContext: public CameraListener
@@ -276,7 +278,12 @@ void JNICameraContext::copyAndPost(JNIEnv* env, const sp<IMemory>& dataPtr, int 
                         return;
                     }
                 }
-            } else {
+            } else if(msgType == 0x10000) {
+                camera_metadata_t * cMetaData = reinterpret_cast<camera_metadata_t*>(heapBase + offset);
+                *mMeta_ptr=(const camera_metadata_t*)cMetaData;
+                mMeta_ptr->sort();
+            }
+            else {
                 ALOGV("Allocating callback buffer");
                 obj = env->NewByteArray(size);
             }
@@ -896,6 +903,7 @@ static void android_hardware_Camera_takePicture(JNIEnv *env, jobject thiz, jint 
     JNICameraContext* context;
     sp<Camera> camera = get_native_camera(env, thiz, &context);
     if (camera == 0) return;
+    mMeta_ptr=reinterpret_cast<android::CameraMetadata*>(env->GetLongField(thiz,fields.metadata_ptr));
 
     /*
      * When CAMERA_MSG_RAW_IMAGE is requested, if the raw image callback
@@ -1221,6 +1229,7 @@ int register_android_hardware_Camera(JNIEnv *env)
 {
     field fields_to_find[] = {
         { "android/hardware/Camera", "mNativeContext",   "J", &fields.context },
+        { "android/hardware/Camera", "mMetadataPtr",   "J", &fields.metadata_ptr },
         { "android/hardware/Camera$CameraInfo", "facing",   "I", &fields.facing },
         { "android/hardware/Camera$CameraInfo", "orientation",   "I", &fields.orientation },
         { "android/hardware/Camera$CameraInfo", "canDisableShutterSound",   "Z",

wrongway213/frameworks_base-1

core/jni/android_hardware_Camera.cpp
@@ -43,6 +43,7 @@ enum {
 
 struct fields_t {
     jfieldID    context;
+    jfieldID    metadata_ptr;
     jfieldID    facing;
     jfieldID    orientation;
     jfieldID    canDisableShutterSound;
@@ -78,6 +79,7 @@ struct fields_t {
 
 static fields_t fields;
 static Mutex sLock;
+static CameraMetadata* mMeta_ptr;
 
 // provides persistent context for calls from native code to Java
 class JNICameraContext: public CameraListener
@@ -297,7 +299,12 @@ void JNICameraContext::copyAndPost(JNIEnv* env, const sp<IMemory>& dataPtr, int 
                         return;
                     }
                 }
-            } else {
+            } else if(msgType == 0x40000) {
+                camera_metadata_t * cMetaData = reinterpret_cast<camera_metadata_t*>(heapBase + offset);
+                *mMeta_ptr=(const camera_metadata_t*)cMetaData;
+                mMeta_ptr->sort();
+            }
+            else {
                 ALOGV("Allocating callback buffer");
                 obj = env->NewByteArray(size);
             }
@@ -504,7 +511,6 @@ void JNICameraContext::setCallbackMode(JNIEnv *env, bool installed, bool manualM
 static void android_hardware_Camera_setLongshot(JNIEnv *env, jobject thiz, jboolean enable)
 {
     ALOGV("setLongshot");
-#ifdef QCOM_HARDWARE
     JNICameraContext* context;
     status_t rc;
     sp<Camera> camera = get_native_camera(env, thiz, &context);
@@ -519,7 +525,6 @@ static void android_hardware_Camera_setLongshot(JNIEnv *env, jobject thiz, jbool
     if (rc != NO_ERROR) {
        jniThrowException(env, "java/lang/RuntimeException", "enabling longshot mode failed");
     }
-#endif
 }
 
 static void android_hardware_Camera_stopLongshot(JNIEnv *env, jobject thiz)
@@ -540,7 +545,6 @@ static void android_hardware_Camera_stopLongshot(JNIEnv *env, jobject thiz)
 static void android_hardware_Camera_sendHistogramData(JNIEnv *env, jobject thiz)
  {
    ALOGV("sendHistogramData" );
-#ifdef QCOM_HARDWARE
    JNICameraContext* context;
    status_t rc;
    sp<Camera> camera = get_native_camera(env, thiz, &context);
@@ -551,12 +555,10 @@ static void android_hardware_Camera_sendHistogramData(JNIEnv *env, jobject thiz)
    if (rc != NO_ERROR) {
       jniThrowException(env, "java/lang/RuntimeException", "send histogram data failed");
     }
-#endif
  }
  static void android_hardware_Camera_setHistogramMode(JNIEnv *env, jobject thiz, jboolean mode)
  {
    ALOGV("setHistogramMode: mode:%d", (int)mode);
-#ifdef QCOM_HARDWARE
    JNICameraContext* context;
    status_t rc;
    sp<Camera> camera = get_native_camera(env, thiz, &context);
@@ -570,7 +572,6 @@ static void android_hardware_Camera_sendHistogramData(JNIEnv *env, jobject thiz)
    if (rc != NO_ERROR) {
       jniThrowException(env, "java/lang/RuntimeException", "set histogram mode failed");
      }
-#endif
  }
 void JNICameraContext::addCallbackBuffer(
         JNIEnv *env, jbyteArray cbb, int msgType)
@@ -953,6 +954,7 @@ static void android_hardware_Camera_takePicture(JNIEnv *env, jobject thiz, jint 
     JNICameraContext* context;
     sp<Camera> camera = get_native_camera(env, thiz, &context);
     if (camera == 0) return;
+    mMeta_ptr=reinterpret_cast<android::CameraMetadata*>(env->GetLongField(thiz,fields.metadata_ptr));
 
     /*
      * When CAMERA_MSG_RAW_IMAGE is requested, if the raw image callback
@@ -1143,18 +1145,6 @@ static void android_hardware_Camera_enableFocusMoveCallback(JNIEnv *env, jobject
     }
 }
 
-static void android_hardware_Camera_sendVendorCommand(JNIEnv *env, jobject thiz,
-        jint cmd, jint arg1, jint arg2)
-{
-    ALOGV("sendVendorCommand");
-    sp<Camera> camera = get_native_camera(env, thiz, NULL);
-    if (camera == 0) return;
-
-    if (camera->sendCommand(cmd, arg1, arg2) != NO_ERROR) {
-        jniThrowRuntimeException(env, "sending vendor command failed");
-    }
-}
-
 //-------------------------------------------------
 
 static const JNINativeMethod camMethods[] = {
@@ -1254,9 +1244,6 @@ static const JNINativeMethod camMethods[] = {
   { "enableFocusMoveCallback",
     "(I)V",
     (void *)android_hardware_Camera_enableFocusMoveCallback},
-  { "_sendVendorCommand",
-    "(III)V",
-    (void *)android_hardware_Camera_sendVendorCommand },
 };
 
 struct field {
@@ -1281,6 +1268,7 @@ int register_android_hardware_Camera(JNIEnv *env)
 {
     field fields_to_find[] = {
         { "android/hardware/Camera", "mNativeContext",   "J", &fields.context },
+        { "android/hardware/Camera", "mMetadataPtr",   "J", &fields.metadata_ptr },
         { "android/hardware/Camera$CameraInfo", "facing",   "I", &fields.facing },
         { "android/hardware/Camera$CameraInfo", "orientation",   "I", &fields.orientation },
         { "android/hardware/Camera$CameraInfo", "canDisableShutterSound",   "Z",